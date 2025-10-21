736930f7e675524b38f5b20f89246e41ee86b6d0
a1pl <157911417+a1pl@users.noreply.github.com>
2025-10-20 21:41:45 -0400

    make an actual shorcut_dir
    xdg_config_home used instead

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,19 @@
 use clap::Parser;
 use std::path::PathBuf;
+use std::env;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(default_value = ".")]
+    #[arg(default_value_t = default_shortcut_dir())]
     pub shortcut_dir: PathBuf
+}
+
+fn default_shortcut_dir() -> PathBuf {
+    let config_home = env::var("XDG_CONFIG_HOME")
+        .unwrap_or_else(|_| {
+            let home = env::var("HOME").unwrap_or_else(|_| String::from("."));
+            format!("{}/.config", home)
+        });
+    PathBuf::from(config_home).join("desktop-tui/shortcuts")
 }
